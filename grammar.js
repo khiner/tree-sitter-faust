80cff53b79402a7e03d9f026a88383b4ec935d92
Karl Hiner <karl.hiner@gmail.com>
2024-01-27 10:16:28 -0500

    Reformat without parens around single arguments (consistent with other grammars)

--- grammar.js
+++ grammar.js
@@ -12,35 +12,35 @@ const PRECEDENCE = {
 
 const sepBy = (sep, rule) => seq(rule, repeat(seq(sep, rule)));
 
-const binaryComposition = (operator, precedence, assoc, field_rule) => ($) =>
+const binaryComposition = (operator, precedence, assoc, field_rule) => $ =>
   prec[assoc](precedence, seq(field('left', field_rule($)), operator, field('right', field_rule($))));
 
 module.exports = grammar({
   name: 'faust',
   rules: {
-    source_file: ($) => repeat(choice($.comment, $._statement)),
+    source_file: $ => repeat(choice($.comment, $._statement)),
 
-    _statement: ($) =>
+    _statement: $ =>
       choice(
         seq(optional(repeat1($.variant)), choice($.global_metadata, $.definition_metadata, $.file_import, $.definition), ';'),
         $.documentation
       ),
 
-    variant: (_) => choice('singleprecision', 'doubleprecision', 'quadprecision', 'fixedpointprecision'),
+    variant: _ => choice('singleprecision', 'doubleprecision', 'quadprecision', 'fixedpointprecision'),
 
-    definition: ($) =>
+    definition: $ =>
       choice(
         seq(field('name', $.identifier), '(', $.params, ')', '=', field('value', $._expression)),
         seq(field('name', $.identifier), '=', field('value', $._expression))
       ),
 
-    _expression: ($) => prec(PRECEDENCE.EXPRESSION, choice($._binary_composition, $._infix)),
-    _infix: ($) =>
+    _expression: $ => prec(PRECEDENCE.EXPRESSION, choice($._binary_composition, $._infix)),
+    _infix: $ =>
       prec(PRECEDENCE.EXPRESSION, choice($.binary_op, seq($._infix, $.one_sample_delay_operator), $.access, $.function_call, $._primitive)),
-    binary_op: ($) => prec.left(PRECEDENCE.BINARY_OP, seq($._infix, $.operator, $._infix)),
+    binary_op: $ => prec.left(PRECEDENCE.BINARY_OP, seq($._infix, $.operator, $._infix)),
     // | infixexp LPAR arglist RPAR       { $$ = buildBoxAppl($1,$3); }
-    function_call: ($) => prec(PRECEDENCE.FUNCTION_CALL, seq($._infix, '(', $.args, ')')),
-    access: ($) => prec(PRECEDENCE.ACCESS, seq($._infix, '.', $.identifier)),
+    function_call: $ => prec(PRECEDENCE.FUNCTION_CALL, seq($._infix, '(', $.args, ')')),
+    access: $ => prec(PRECEDENCE.ACCESS, seq($._infix, '.', $.identifier)),
     // [$._infix, $.operator, $._expression, '.', $.identifier],
     // function_call: ($) =>
     // prec(
@@ -48,7 +48,7 @@ module.exports = grammar({
     //   seq(optional(seq(alias($.identifier, $.module_name), '.')), alias($.identifier, $.function_name), '(', sepBy(',', $._argument), ')')
     // ),
 
-    _primitive: ($) =>
+    _primitive: $ =>
       choice(
         $._number,
         $.wire,
@@ -64,21 +64,21 @@ module.exports = grammar({
         $.iteration
       ),
 
-    args: ($) => sepBy(',', $._argument),
-    params: ($) => sepBy(',', alias($.identifier, $.parameter)),
+    args: $ => sepBy(',', $._argument),
+    params: $ => sepBy(',', alias($.identifier, $.parameter)),
 
     // All arguments are direct children of `args`, so we make this rule hidden.
-    _argument: ($) => choice($.sequential_arg, $.split_arg, $.merge_arg, $.recursive_arg, $._infix),
+    _argument: $ => choice($.sequential_arg, $.split_arg, $.merge_arg, $.recursive_arg, $._infix),
     // Binary compositions are restricted to non-parallel compositions to avoid ambiguity with commas.
     // Note: Can we allow parallel compositions in arguments when surrounded by parentheses?
     // This could be contributed to Faust.
-    recursive_arg: binaryComposition('~', PRECEDENCE.RECURSIVE, 'left', ($) => $._argument),
-    sequential_arg: binaryComposition(':', PRECEDENCE.SEQ, 'right', ($) => $._argument),
-    split_arg: binaryComposition('<:', PRECEDENCE.SPLIT, 'right', ($) => $._argument),
-    merge_arg: binaryComposition(':>', PRECEDENCE.MERGE, 'right', ($) => $._argument),
+    recursive_arg: binaryComposition('~', PRECEDENCE.RECURSIVE, 'left', $ => $._argument),
+    sequential_arg: binaryComposition(':', PRECEDENCE.SEQ, 'right', $ => $._argument),
+    split_arg: binaryComposition('<:', PRECEDENCE.SPLIT, 'right', $ => $._argument),
+    merge_arg: binaryComposition(':>', PRECEDENCE.MERGE, 'right', $ => $._argument),
 
     // todo test
-    iteration: ($) =>
+    iteration: $ =>
       seq(
         choice('par', 'seq', 'sum', 'prod'),
         '(',
@@ -90,7 +90,7 @@ module.exports = grammar({
         ')'
       ),
 
-    operator: (_) =>
+    operator: _ =>
       choice(
         // Math
         '+',
@@ -118,17 +118,17 @@ module.exports = grammar({
 
     wire: () => '_',
     cut: () => '!',
-    one_sample_delay_operator: (_) => "'",
+    one_sample_delay_operator: _ => "'",
 
-    _number: ($) => choice($.int, $.real),
-    int: (_) => {
+    _number: $ => choice($.int, $.real),
+    int: _ => {
       const decimal = /[0-9]/;
       const sign = optional(/[+-]/);
       const int_literal = repeat1(decimal);
 
       return token(seq(sign, int_literal));
     },
-    real: (_) => {
+    real: _ => {
       const decimal = /[0-9]/;
       const float_literal = choice(
         seq(repeat1(decimal), 'f'),
@@ -142,32 +142,32 @@ module.exports = grammar({
       return token(seq(sign, float_literal));
     },
 
-    documentation: ($) => seq('<mdoc>', repeat($._doc_content), '</mdoc>'),
-    _doc_content: ($) => choice($._doc_char, $._special_doc_tag),
-    _doc_char: (_) => /[^<]+/,
-    _special_doc_tag: ($) =>
+    documentation: $ => seq('<mdoc>', repeat($._doc_content), '</mdoc>'),
+    _doc_content: $ => choice($._doc_char, $._special_doc_tag),
+    _doc_char: _ => /[^<]+/,
+    _special_doc_tag: $ =>
       choice('<notice/>', '<notice />', '<equation>', '</equation>', '<diagram>', '</diagram>', '<metadata>', '</metadata>', '<listing'),
 
-    file_import: ($) => seq('import', '(', $.string, ')'),
+    file_import: $ => seq('import', '(', $.string, ')'),
 
-    global_metadata: ($) => seq('declare', alias($.identifier, $.metadata_key), $.string),
-    definition_metadata: ($) => seq('declare', alias($.identifier, $.function_name), alias($.identifier, $.metadata_key), $.string),
+    global_metadata: $ => seq('declare', alias($.identifier, $.metadata_key), $.string),
+    definition_metadata: $ => seq('declare', alias($.identifier, $.function_name), alias($.identifier, $.metadata_key), $.string),
 
-    _binary_composition: ($) => choice($.recursive, $.sequential, $.split, $.merge, $.parallel),
-    recursive: binaryComposition('~', PRECEDENCE.RECURSIVE, 'left', ($) => $._expression),
-    sequential: binaryComposition(':', PRECEDENCE.SEQ, 'right', ($) => $._expression),
-    split: binaryComposition('<:', PRECEDENCE.SPLIT, 'right', ($) => $._expression),
-    merge: binaryComposition(':>', PRECEDENCE.MERGE, 'right', ($) => $._expression),
-    parallel: binaryComposition(',', PRECEDENCE.PAR, 'right', ($) => $._expression),
+    _binary_composition: $ => choice($.recursive, $.sequential, $.split, $.merge, $.parallel),
+    recursive: binaryComposition('~', PRECEDENCE.RECURSIVE, 'left', $ => $._expression),
+    sequential: binaryComposition(':', PRECEDENCE.SEQ, 'right', $ => $._expression),
+    split: binaryComposition('<:', PRECEDENCE.SPLIT, 'right', $ => $._expression),
+    merge: binaryComposition(':>', PRECEDENCE.MERGE, 'right', $ => $._expression),
+    parallel: binaryComposition(',', PRECEDENCE.PAR, 'right', $ => $._expression),
 
-    string: (_) => /"([^"\\]|\\.)*"/,
+    string: _ => /"([^"\\]|\\.)*"/,
 
     // todo Improve qualified identifiers, using a visible rule for 'qualified_identifier'
     //   See 'qualified_identifier' in https://github.com/tree-sitter/tree-sitter-cpp/blob/master/grammar.js,
     //   and the test: https://github.com/tree-sitter/tree-sitter-cpp/blob/master/test/corpus/statements.txt#L409-L425
-    identifier: ($) => prec.right(seq(optional('::'), seq($._id, repeat(seq('::', $._id))))),
-    _id: (_) => /_*[a-zA-Z][_a-zA-Z0-9]*/,
+    identifier: $ => prec.right(seq(optional('::'), seq($._id, repeat(seq('::', $._id))))),
+    _id: _ => /_*[a-zA-Z][_a-zA-Z0-9]*/,
 
-    comment: (_) => token(choice(seq('//', /(\\(.|\r?\n)|[^\\\n])*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))),
+    comment: _ => token(choice(seq('//', /(\\(.|\r?\n)|[^\\\n])*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))),
   },
 });
